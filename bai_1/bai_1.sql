34fc23edea7f6d0cab54b8b62336f7dbd5698383
xuantuong97 <tieutho2026@gmail.com>
2023-10-11 15:13:49 +0700

    l

--- bai_1/bai_1.sql
+++ bai_1/bai_1.sql
@@ -1,12 +1,12 @@
 create database student_management;
 use student_management;
 
-create table Class (
+create table class (
 id int primary key auto_increment,
 `name` varchar(50)
 );
 
-create table Teacher (
+create table teacher (
 id int primary key auto_increment,
 `name` varchar(50),
 age int not null,
